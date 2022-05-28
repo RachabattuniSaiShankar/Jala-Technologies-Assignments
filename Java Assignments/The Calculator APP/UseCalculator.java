8fbbd6bb866a7aaac7e856e25716ff0c5ff56e28
RachabattuniSaiShankar <106427186+RachabattuniSaiShankar@users.noreply.github.com>
2022-05-28 18:30:00 +0530

    Add files via upload

---
+++ Java Assignments/The Calculator APP/UseCalculator.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+// Class Definition
+class UseCalculator
+{
+	public static void main(String[] args) throws java.io.IOException
+	{
+		BufferedReader buffer
+			= new BufferedReader(new InputStreamReader(System.in));
+
+		// Gets User Input
+		System.out.println("Select the Calculator: Basic - B or Scientific - S.");
+		System.out.flush();
+		String option=buffer.readLine();
+
+		if(option.length()==1)
+		{
+			if (option.equals("B") || option.equals("b"))
+			{
+				// Calls the Basic Calculator Application
+				Calculator c=new Calculator();
+				c.Calc();
+			}
+			else if(option.equals("S") || option.equals("s"))
+			{
+				// Calls the Scientific Calculator Application
+				ScientificCalculator sc=new ScientificCalculator();                                                     
+				sc.Calc();
+			}
+			else
+			{	
+				System.out.println("***Please enter option 'B' or 'S'.***");
+			}
+		}
+		else
+		{
+			System.out.println("***Please enter option 'B' or 'S'.***");
+		}
+	}
+}
